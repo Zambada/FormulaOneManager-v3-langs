c0cca75fc5ab4331b6987ace5ac5ed4420932654
Zambada <marco30100@gmail.com>
2012-07-24 18:12:56 +0200

    deuxieme essais

--- fr/admin.php
+++ fr/admin.php
@@ -1,3 +1,3 @@
 <?php
 
-[17:45:07] Charles Duprey: $lang['BIENVENUE_ADMIN'] = "Bienvenue sur l'interface d'administration !";
+ $lang['BIENVENUE_ADMIN'] = "Bienvenue sur l'interface d'administration !";
